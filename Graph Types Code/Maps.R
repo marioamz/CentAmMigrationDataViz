3d9f54a60d12e3455a5d23864cabef5603edaf83
Mario Moreno <marioamz@gmail.com>
2019-02-07 21:24:31 -0600

    shape files and maps

---
+++ Graph Types Code/Maps.R
@@ -0,0 +1,118 @@
+install.packages('sf')
+install.packages('tigris')
+library(sf)
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(tigris)
+library(gtools)
+
+# Read in departmental level shape file 
+honduras_shape <- st_read('Data/HND_adm/HND_adm1.shp')
+guate_shape <- st_read('Data/gadm36_GTM_shp/gadm36_GTM_1.shp')
+els_shape <- st_read('Data/gadm36_SLV_shp/gadm36_SLV_1.shp')
+
+# Read in Honduras data
+els <- read.csv('Data/elsLAPOP.csv', encoding = 'latin1', stringsAsFactors = FALSE)
+guate <- read.csv('Data/guateLAPOP.csv', encoding='latin1', stringsAsFactors = FALSE)
+hond <- read.csv('Data/hondLAPOP.csv', encoding='latin1', stringsAsFactors = FALSE)
+
+els$pais[els$pais==3] <- "El Salvador"
+guate$pais[guate$pais==2] <- "Guatemala"
+hond$pais[hond$pais==4] <- "Honduras"
+
+cols_to_keep <- intersect(colnames(els), colnames(guate))
+g <- guate[, cols_to_keep, drop=FALSE]
+e <- els[, cols_to_keep, drop=FALSE]
+bind <- smartbind(g, e)
+cols_to_keep <- intersect(colnames(bind), colnames(hond))
+h <- hond[, cols_to_keep, drop=FALSE]
+b <- bind[, cols_to_keep, drop=FALSE]
+final <- smartbind(h, b)
+
+rm(cols_to_keep, g, e, h, b, bind)
+
+# Subset to determine what changes are over time of people wanting to emigrate
+
+subemigrate <- final %>%
+  select(year, pais, prov, q14) %>%
+  filter(year != 2012, year != 2014, prov != 414, prov != 415, prov != 416, prov != 417, prov != 219) %>%
+  na.omit() %>%
+  group_by(year, pais, prov) %>%
+  add_tally() %>%
+  add_count(q14) %>%
+  distinct(year, pais, prov, q14, n, nn)
+
+subemigrate$perc <- (subemigrate$nn / subemigrate$n) * 100
+
+emigrate <- subemigrate %>%
+  select(year, pais, prov, perc, q14) %>%
+  group_by(year, pais, prov) %>%
+  arrange(q14) %>%
+  slice(1)  %>%
+  select(year, pais, prov, perc) %>%
+  group_by(year, pais, prov) %>%
+  arrange(desc(perc))
+
+emigrate$prov[emigrate$prov=='AtlÃ¡ntida'] <- "Atlántida"
+emigrate$prov[emigrate$prov=='ColÃ³n'] <- "Colón"
+emigrate$prov[emigrate$prov=='CopÃ¡n'] <- "Copán"
+emigrate$prov[emigrate$prov=='CortÃ©s'] <- "Cortés"
+emigrate$prov[99] <- "El Paraíso"
+emigrate$prov[emigrate$prov=='Francisco MorazÃ¡n'] <- "Francisco Morazán"
+emigrate$prov[emigrate$prov=='IntibucÃ¡'] <- "Intibucá"
+emigrate$prov[19] <- "Islas de la Bahía"
+emigrate$prov[emigrate$prov=='Santa BÃ¡rbara'] <- "Santa Bárbara"
+emigrate$prov[emigrate$prov=='SONSONATE'] <- "Sonsonate"
+emigrate$prov[emigrate$prov=='LA UNION'] <- "La Unión"
+emigrate$prov[emigrate$prov=='CUSCATLAN'] <- "Cuscatlán"
+emigrate$prov[emigrate$prov=='CABAÑAS'] <- "Cabañas"
+emigrate$prov[emigrate$prov=='QuichÃ©'] <- "Quiché"
+emigrate$prov[emigrate$prov=='SAN VICENTE'] <- "San Vicente"
+emigrate$prov[emigrate$prov=='LA LIBERTAD'] <- "La Libertad"
+emigrate$prov[emigrate$prov=='SANTA ANA'] <- "Santa Ana"
+emigrate$prov[emigrate$prov=='CuscatlÃ¡n'] <- "Cuscatlán"
+emigrate$prov[emigrate$prov=='LA PAZ'] <- "La Paz"
+emigrate$prov[emigrate$prov=='AHUACHAPAN'] <- "Ahuachapán"
+emigrate$prov[emigrate$prov=='SAN MIGUEL'] <- "San Miguel"
+emigrate$prov[emigrate$prov=='USULUTAN'] <- "Usulután"
+emigrate$prov[emigrate$prov=='MORAZAN'] <- "Morazán"
+emigrate$prov[emigrate$prov=='La UniÃ³n'] <- "La Unión"
+emigrate$prov[emigrate$prov=='UsulutÃ¡n'] <- "Usulután"
+emigrate$prov[emigrate$prov=='SuchitepÃ©quez'] <- "Suchitepéquez"
+emigrate$prov[emigrate$prov=='CHALATENANGO'] <- "Chalatenango"
+emigrate$prov[emigrate$prov=='CabaÃ±as'] <- "Cabañas"
+emigrate$prov[emigrate$prov=='SacatepÃ©quez'] <- "Sacatepéquez"
+emigrate$prov[emigrate$prov=='TotonicapÃ¡n'] <- "Totonicapán"
+emigrate$prov[emigrate$prov=='AhuachapÃ¡n'] <- "Ahuachapán"
+emigrate$prov[emigrate$prov=='SololÃ¡'] <- "Sololá"
+emigrate$prov[emigrate$prov=='MorazÃ¡n'] <- "Morazán"
+emigrate$prov[emigrate$prov=='PetÃ©n'] <- "Petén"
+emigrate$prov[emigrate$prov=='SAN SALVADOR'] <- "San Salvador"
+
+
+diff <- emigrate %>%
+  spread(year, perc) %>%
+  group_by(prov) 
+
+colnames(diff)[colnames(diff)==2010] <- "perc2010"
+colnames(diff)[colnames(diff)==2016] <- "perc2016"
+
+diff_use <- diff %>%
+  mutate(change = perc2016 - perc2010) %>%
+  select(prov, change)
+
+
+to_map_hond <- geo_join(honduras_shape, diff_use, by_sp = 'NAME_1', by_df='prov', how='left')
+to_map_guate <- geo_join(guate_shape, diff_use, by_sp='NAME_1', by_df='prov', how='left')
+to_map_els <- geo_join(els_shape, diff_use, by_sp='NAME_1', by_df='prov', how='left')
+
+ggplot() +
+  geom_sf(data = to_map_hond) + 
+  geom_sf(data = to_map_hond, aes(fill=change))
+
+#+
+ # geom_sf(data = to_map_guate) +
+  #geom_sf(data = to_map_guate, aes(fill=change)) + 
+  #geom_sf(data = to_map_els) + 
+  #geom_sf(data = to_map_els, aes(fill=change))
