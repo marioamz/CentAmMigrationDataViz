442957242aaf936bedceb5d2c24c4933233588aa
Mario Moreno <marioamz@gmail.com>
2019-02-15 19:16:52 -0600

    updates

--- Graph Types Code/Cleveland.R
+++ Graph Types Code/Cleveland.R
@@ -7,113 +7,220 @@ library(tidyverse)
 library(gtools)
 
 ## Read in data
-els <- read.csv('Data/elsLAPOP.csv', encoding = 'latin1')
-guate <- read.csv('Data/guateLAPOP.csv', encoding='latin1')
 hond <- read.csv('Data/hondLAPOP.csv', encoding='latin1')
-
-els$pais[els$pais==3] <- "El Salvador"
-guate$pais[guate$pais==2] <- "Guatemala"
 hond$pais[hond$pais==4] <- "Honduras"
-
-cols_to_keep <- intersect(colnames(els), colnames(guate))
-g <- guate[, cols_to_keep, drop=FALSE]
-e <- els[, cols_to_keep, drop=FALSE]
-bind <- smartbind(g, e)
-cols_to_keep <- intersect(colnames(bind), colnames(hond))
-h <- hond[, cols_to_keep, drop=FALSE]
-b <- bind[, cols_to_keep, drop=FALSE]
-final <- smartbind(h, b)
-
-rm(cols_to_keep, g, e, h, b, bind)
-
-## Subset to have emigration question (aoj11)
-
-subemigrate <- hond %>%
-  select(year, prov, q14) %>%
-  filter(year != 2012, year != 2014, prov != 414, prov != 415, prov != 416, prov != 417) %>%
-  na.omit() %>%
-  group_by(year, prov) %>%
-  add_tally() %>%
-  add_count(q14) %>%
-  distinct(year, prov, q14, n, nn)
-
-subemigrate$perc <- (subemigrate$nn / subemigrate$n) * 100
-
-emigrate <- subemigrate %>%
-  select(year, prov, perc, q14) %>%
-  group_by(year, prov) %>%
-  arrange(q14) %>%
-  slice(1)  %>%
-  select(year, prov, perc) %>%
-  group_by(year, prov) %>%
-  arrange(desc(perc))
-
-
-
-## Stupid encodings don't work
-
-emigrate$prov[emigrate$prov=='AtlÃ¡ntida'] <- "Atlántida"
-emigrate$prov[emigrate$prov=='ColÃ³n'] <- "Colón"
-emigrate$prov[emigrate$prov=='CopÃ¡n'] <- "Copán"
-emigrate$prov[emigrate$prov=='CortÃ©s'] <- "Cortés"
-emigrate$prov[33] <- "El Paraíso"
-emigrate$prov[emigrate$prov=='Francisco MorazÃ¡n'] <- "Francisco Morazán"
-emigrate$prov[emigrate$prov=='IntibucÃ¡'] <- "Intibucá"
-emigrate$prov[11] <- "Islas de la Bahía"
-emigrate$prov[emigrate$prov=='Santa BÃ¡rbara'] <- "Santa Bárbara"
-
-## Plot
-
-# to add labels to plot
-right_label <- emigrate %>%
+hond2016 <- hond %>%
   filter(year == 2016)
 
-left_label <- emigrate %>%
-  filter(year == 2010)
+## Subset to have just the variables we care about
 
-# to highlight only those greater than 30%
-diff <- emigrate %>%
-  spread(year, perc) %>%
-  group_by(prov) 
+touse <- hond2016 %>%
+  select(q14, ur, q1, q2, idio2, it1, l1, aoj11, aoj17, aoj12, ed) %>%
+  na.omit() 
 
-colnames(diff)[colnames(diff)==2010] <- "perc2010"
-colnames(diff)[colnames(diff)==2016] <- "perc2016"
+## Create bins
+### For age, 1 is less than 35 and 2 is greater than 35
+touse$q2 <- cut(touse$q2, breaks = c(0, 35, Inf), include.lowest=TRUE, labels=c(1, 2))
+### For personal economic situation, 1 is worse off than before, 2 is better off
+touse$idio2 <- cut(touse$idio2, breaks=c(0, 2, Inf), include.lowest=TRUE, labels=c(2,1))
+### For thinking the neighborhood is unsafe, 1 is unsafe and 2 is not
+touse$it1 <- cut(touse$it1, breaks=c(0, 2, Inf), include.lowest=TRUE, labels=c(2,1))
+### For political leaning, 1 is right wing and 2 is moderate or left wing
+touse$l1 <- cut(touse$l1, breaks=c(0, 5, Inf), include.lowest=TRUE, labels=c(2,1))
+### For confidence in judicial system if robbed, 1 is not confident, 2 is confident
+touse$aoj12 <- cut(touse$aoj12, breaks=c(0, 2, Inf), include.lowest=TRUE, labels=c(2,1))
+### For those who finished elementary school, 1 is not finished, 2 is finished
+touse$ed <- cut(touse$ed, breaks=c(0, 6, Inf), include.lowest=TRUE, labels=c(1,2))
+### Rural is 1, urban is 2
+touse$ur<-as.numeric(touse$ur) 
+
+## Let's get percentages of all 
+percs <- touse %>%
+  group_by(q14) %>%
+  add_tally() %>%
+  add_count(q2) 
+percs$age <- (percs$nn / percs$n) * 100
+
+percs <- percs %>%
+  select (-c(n, nn)) %>%
+  group_by(q14) %>%
+  add_tally() %>%
+  add_count(ur)
+percs$rural <- (percs$nn / percs$n) * 100
+
+percs <- percs %>%
+  select (-c(n, nn)) %>%
+  group_by(q14) %>%
+  add_tally() %>%
+  add_count(q1)
+percs$gender <- (percs$nn / percs$n) * 100
+
+percs <- percs %>%
+  select (-c(n, nn)) %>%
+  group_by(q14) %>%
+  add_tally() %>%
+  add_count(idio2)
+percs$econ <- (percs$nn / percs$n) * 100
+
+percs <- percs %>%
+  select (-c(n, nn)) %>%
+  group_by(q14) %>%
+  add_tally() %>%
+  add_count(it1)
+percs$hood <- (percs$nn / percs$n) * 100
+
+percs <- percs %>%
+  select (-c(n, nn)) %>%
+  group_by(q14) %>%
+  add_tally() %>%
+  add_count(l1)
+percs$politics <- (percs$nn / percs$n) * 100
+
+percs <- percs %>%
+  select (-c(n, nn)) %>%
+  group_by(q14) %>%
+  add_tally() %>%
+  add_count(aoj12)
+percs$justice <- (percs$nn / percs$n) * 100
+
+percs <- percs %>%
+  select (-c(n, nn)) %>%
+  group_by(q14) %>%
+  add_tally() %>%
+  add_count(ed)
+percs$education <- (percs$nn / percs$n) * 100
+
+
+## Pull out only those who answered 1 per question
+
+rural <- percs %>%
+  group_by(q14) %>%
+  distinct(q14, ur, rural) %>%
+  slice(1) %>%
+  select(q14, rural)
+
+age <- percs %>%
+  group_by(q14) %>%
+  distinct(q14, q2, age) %>%
+  arrange(q2) %>%
+  slice(1) %>%
+  select(q14, age)
+
+econ <- percs %>%
+  group_by(q14) %>%
+  distinct(q14, idio2, econ) %>%
+  arrange(desc(idio2)) %>%
+  slice(1) %>%
+  select(q14, econ)
+
+politics <- percs %>%
+  group_by(q14) %>%
+  distinct(q14, l1, politics) %>%
+  arrange(desc(l1)) %>%
+  slice(1) %>%
+  select(q14, politics)
+
+neighborhood <- percs %>%
+  group_by(q14) %>%
+  distinct(q14, it1, hood) %>%
+  arrange(desc(it1)) %>%
+  slice(1) %>%
+  select(q14, hood)
+
+confidence <- percs %>%
+  group_by(q14) %>%
+  distinct(q14, aoj12, justice) %>%
+  arrange(desc(aoj12)) %>%
+  slice(1) %>%
+  select(q14, justice)
+
+elementary <- percs %>%
+  group_by(q14) %>%
+  distinct(q14, ed, education) %>%
+  arrange(ed) %>%
+  slice(1) %>%
+  select(q14, education)
+
+
+
+percentages <- list(age, confidence, econ, elementary, neighborhood, politics, rural) %>%
+  reduce(left_join, by='q14')
+
+percentages <- as.data.frame(t(percentages))
+colnames(percentages) = percentages[1, ] # the first row will be the header
+percentages = percentages[-1, ]   
+names(percentages) <- c("emigrate", "not_emigrate")
+percentages <- cbind(answers = rownames(percentages), percentages)
+rownames(percentages) <- 1:nrow(percentages)
+
+percentages <- percentages %>%
+  mutate(change = emigrate - not_emigrate) %>%
+  arrange(desc(change))
   
-diff_use <- diff %>%
-  mutate(change = perc2016 - perc2010) %>%
-  arrange(change) %>%
-  filter(change > 30)
 
-right_label <- right_label %>%
-  filter(prov %in% diff_use$prov)
+emigratepos <- percentages %>%
+  filter(change > 0) %>%
+  arrange(desc(change))
 
-left_label <- left_label %>%
-  filter(prov %in% diff_use$prov)
+emigrateneg <- percentages %>%
+  filter(change < 0) %>%
+  arrange(desc(change))
 
-highlight <- emigrate %>%
-  filter(prov %in% diff_use$prov)
+percentages$answers <- factor(percentages$answers, levels = percentages$answers[order(percentages$change)])
 
-change <- merge(right_label, diff_use, by='prov')
-
+colors <- c('c1' = '#67001f', 'c2' = '#df65b0')
 # Actually plot
 
-plot <- ggplot(emigrate, aes(perc, prov)) +
-  geom_point(colour = alpha('grey', 0.7)) +
-  geom_line(aes(group=prov), colour = alpha('grey', 0.7)) + 
-  geom_line(data = highlight, aes(group = prov)) +
-  geom_point(data = highlight, aes(color = year), size = 2) +
-  scale_color_gradient(low='#df65b0', high='#67001f') +
-  geom_text(data = change, aes(label = paste0('+', round(change, 0))),
-            size = 3, hjust = -.5) 
+plot <- ggplot(percentages, aes(emigrate, answers)) +
+  geom_point(data = emigratepos, aes(emigrate),color = '#67001f', size =4) + 
+  geom_point(data = emigrateneg, aes(emigrate), color = '#67001f', size =4) + 
+  geom_point(data = emigratepos, aes(not_emigrate), color = '#df65b0', size=4) +
+  geom_point(data = emigrateneg, aes(not_emigrate), color = '#df65b0', size=4) +
+  geom_text(data = emigratepos, aes(label=paste0(round(emigrate, 0), '%')), color = '#67001f', hjust = -1, size = 3,
+            position = position_dodge(width = 1),
+            inherit.aes = TRUE) +
+  geom_text(data = emigrateneg, aes(label=paste0(round(emigrate, 0), '%')), color = '#67001f', hjust = 2, size = 3,
+            position = position_dodge(width = 1),
+            inherit.aes = TRUE) +
+  geom_segment(data = emigratepos, aes(x=emigrate, xend=not_emigrate, y=answers, yend=answers), color = '#67001f', size=1) + 
+  geom_segment(data = emigrateneg, aes(x=emigrate, xend=not_emigrate, y=answers, yend=answers), color = '#df65b0', size=1) +
+  expand_limits(x=0) +
+  geom_text(data = emigratepos, aes(x = not_emigrate, y= answers, label=paste0(round(not_emigrate, 0), '%')), color = '#df65b0', hjust = 1.5, size = 3,
+            position = position_dodge(width = 1)) +
+  geom_text(data = emigrateneg, aes(x = not_emigrate, y=answers, label=paste0(round(not_emigrate, 0), '%')), color = '#df65b0', hjust = -1.25, size = 3,
+            position = position_dodge(width = 1)) +
+  geom_label(data = emigratepos, aes(label=c('% who are younger than 35',
+                                             '% who do not trust neighbors', 
+                                             '% whose economic situation has gotten worse in last year', 
+                                             '% who do not trust the justice system to prosecute a robbery')), position=position_nudge(-35), colour = '#67001f', fontface = 'bold') +
+  geom_label(data = emigrateneg, aes(label=c('% who live in rural communities',
+                                             '% who self-identify as conservative', 
+                                             '% who did not complete elementary school ')), position=position_nudge(-30), colour = '#df65b0', fontface = 'bold') +
+  scale_color_manual(name="Hondurans", 
+                     breaks=c('c1', 'c2'), 
+                     values = colors,
+                     labels = c('Planning to Emigrate', 'Not Planning to Emigrate'))
 
-labels <- labs(title = "Most Honduran provinces saw increases in \n respondents planning to emigrate from 2010 to 2016",
-     subtitle = "Out of 18 provinces, three experienced a 30% or greater increase \nin respondents planning to emigrate in the next three years.") 
+labels <- labs(title = "Hondurans planning to emigrate (dark purple) are more \n likely report living in unsafe, gang-filled neighborhoods \n than those who don't (light purple)",
+     subtitle = "Surprisingy, those who plan to emigrate are more likely to have finished elementary school, live in a city, be younger, \n and be less politically conservative than those who don't.") 
 
 update_themes <- 
   theme(axis.title = element_blank(),
         panel.grid.major.x = element_blank(),
         panel.grid.minor = element_blank(),
-        legend.position = 'none') 
+        panel.background = element_blank(),
+        axis.text.y = element_blank(), 
+        axis.ticks = element_blank())
 
-plot + labels + theme_minimal() + mario_theme + update_themes
+mario_theme <- theme(text = element_text(family='Georgia'),
+                     plot.title = element_text(size = 20, margin = margin(b = 10)),
+                     plot.subtitle = element_text(size = 10, color = "darkslategrey", margin = margin(b = 25)), 
+                     #axis.title.x = element_text(color='darkslategrey', size=8),
+                     #axis.title.y = element_text(color='darkslategrey', size=8), 
+                     #plot.caption = element_text(size = 8, margin = margin(t = 10), color = "grey70", hjust = 0),
+                     #legend.title = element_text(face='bold', size=8),
+                     #legend.position = 'bottom', legend.direction = 'horizontal', legend.box = 'horizontal', 
+                     legend.key.size = unit(0.5, 'cm'))
+
+plot + labels  + mario_theme + update_themes
 
